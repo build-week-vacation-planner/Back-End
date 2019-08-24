be6fb3c7c35dab27375261bc4a86c0920862f4b4
tthompson082 <tthompson082@gmail.com>
2019-08-24 10:01:34 -0400

    Initial Commit

---
+++ vacationplanner/src/main/java/com/lambdaschool/vacationplanner/VacationplannerApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.vacationplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationplannerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(VacationplannerApplication.class, args);
+    }
+
+}
